f7bbf65ad6c1dc938547fa3622ebcbeeeb118726
Lorenzo Trojan <lorenzo.trojan@gmail.com>
2018-04-05 12:50:39 +0100

    simple setup.py file

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+
+version = '0.0.1'
+local_dir = os.path.dirname(__file__)
+if len(local_dir) == 0:
+    local_dir = '.'
+try:
+    with open(os.sep.join([local_dir, 'VERSION']), 'r') as fid:
+        version = fid.read().strip()
+except:
+    pass
+
+
+setup(
+    name='simple_search',
+    version=version,
+    description='Simple Search',
+    packages=[
+        "simple_search",
+    ],
+)
